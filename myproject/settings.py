08c78c381b50703b7d936afc41afbc822a2758f8
Damandeep Singh <damandeepsingh1999@gmail.com>
2018-06-03 13:13:47 +0000

    Add requirements.txt file

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-from decouple import config
+
 import os
 import dj_database_url
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -33,7 +34,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
-
+}
 
 # Application definition
 
