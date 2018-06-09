68082d5e35d17c8a980423afd97cf9302b249e2e
Damandeep Singh <damandeepsingh1999@gmail.com>
2018-06-09 11:46:29 +0000

    Add requirements.txt file

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,7 +30,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        default=os.environ['DATABASE_URL']
     )
 }
 
